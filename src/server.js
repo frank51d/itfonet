55fee2a61616ef6c55226691702f829939cc211e
frank51d <frank52xd@gmail.com>
2020-02-11 11:10:42 -0400

    v-6

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/fonetApp'));
+app.use(express.static('./dist/fonetApp'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
 });
