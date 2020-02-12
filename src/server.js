a4ce9b8b48ceba018f9ccf5276764748f4cf03fd
Francisco Perez <frank51xd@gmail.com>
2020-02-12 01:13:50 -0400

    Godver

--- src/server.js
+++ src/server.js
@@ -6,16 +6,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-    req.header("Access-Control-Allow-Origin", "*");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.use(express.static('./dist/fonetApp'));
+app.use(express.static(__dirname+'/dist/fonetApp'));
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
@@ -23,11 +14,4 @@ app.get('/',function(req,res){
 
 app.listen(process.env.PORT || 8080);
 
-app.use(function(req, res, next) {
-    req.header("Access-Control-Allow-Origin", "*");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
