a985b419def58e99f591cd211e54043df9fb3112
Francisco Perez <frank51xd@gmail.com>
2020-02-11 20:55:49 -0400

    v-df

--- src/server.js
+++ src/server.js
@@ -6,11 +6,22 @@ const app = express();
 
 app.use(cors());
 
-app.options('*', cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
 
 app.use(express.static('./dist/fonetApp'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
 });
+app.get('/auth/login', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 app.listen(process.env.PORT || 8080);
