1dcc3a77fc9c6352b3308c7d72e60cf010992a54
Francisco Perez <frank51xd@gmail.com>
2020-02-11 21:14:20 -0400

    v-pls

--- src/server.js
+++ src/server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
     res.header(
@@ -20,8 +18,8 @@ app.use(express.static('./dist/fonetApp'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
 });
-app.get('/auth/login', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-})
+
 
 app.listen(process.env.PORT || 8080);
+
+app.use(cors());
